c7281f5446852cdb6ee609ca5a8d234d411866d8
SeemantaDebdas <seemantadebdas@gmail.com>
2021-10-07 04:34:02 +0530

    Almost Finished Weapon.

--- Assets/RPG Adventures/Scripts/DamageSystem/WeaponDamage.cs
+++ Assets/RPG Adventures/Scripts/DamageSystem/WeaponDamage.cs
@@ -4,5 +4,30 @@ using UnityEngine;
 
 public class WeaponDamage : MonoBehaviour
 {
+    [System.Serializable]
+    public class AttackPoints
+    {
+        public Transform rootPosition;
+        public float radius;
+        public Vector3 offset;
+    }
+
+    public AttackPoints[] attackPoints = new AttackPoints[0];
+
     public int weaponDamage;
+
+#if UNITY_EDITOR
+    private void OnDrawGizmosSelected()
+    {
+        foreach(AttackPoints attackPoint in attackPoints)
+        {
+            if (attackPoint.rootPosition != null)
+            {
+                Vector3 localOffset = attackPoint.rootPosition.TransformVector(attackPoint.offset);
+                Gizmos.color = new Color(1.0f, 1.0f, 1.0f, 0.5f);
+                Gizmos.DrawSphere(attackPoint.rootPosition.position + localOffset, attackPoint.radius);
+            }
+        }      
+    }
+#endif
 }
