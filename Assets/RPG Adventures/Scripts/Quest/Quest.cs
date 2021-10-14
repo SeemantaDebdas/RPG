6ab9960126c687b09988482bcec1d55d6b21f8b4
SeemantaDebdas <seemantadebdas@gmail.com>
2021-10-15 00:41:48 +0530

    Started Quest System

---
+++ Assets/RPG Adventures/Scripts/Quest/Quest.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum QuestType
+{
+    HUNT,
+    GATHER,
+    TALK,
+    EXPLORE
+}
+
+[System.Serializable]
+public class Quest 
+{
+    public string uID;
+    public string title;
+    public string description;
+    public int experience;
+    public int gold;
+
+    public int amount;
+    public string[] target;
+
+    public string talkTo;
+    public Vector3 explore;
+
+    public string questOwner;
+    public QuestType type;
+}
