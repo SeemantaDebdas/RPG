c0034e22c9dfe3658dda74992f94bccbd4afeac4
SeemantaDebdas <seemantadebdas@gmail.com>
2021-10-15 20:11:15 +0530

    Raycast Pointer Hit Finished

--- Assets/RPG Adventures/Scripts/Player/CharacterInput.cs
+++ Assets/RPG Adventures/Scripts/Player/CharacterInput.cs
@@ -7,53 +7,56 @@ namespace RPG {
     {
 
         [SerializeField] float attackRate = 0.3f;
+        [SerializeField] float interactableDistance = 10f;
 
-        Vector3 input;
+        Vector3 movementInput;
         bool canSprint;
         bool isAttacking;
+        bool isLeftMouseClick;
+        bool isInteracting;
+        bool isTalking;
 
-        public Vector3 GetInput
-        {
-            get { 
-                return input;
-            }
-        }
+        public Vector3 GetInput { get { return movementInput; } }
+        public bool IsAttacking { get { return isAttacking; } }
+        public bool IsInput { get { return !Mathf.Approximately(movementInput.magnitude, 0f); } }
+        public bool CanSprint { get { return canSprint; } }
 
-        public bool IsAttacking
-        {
-            get { return isAttacking; }
-        }
 
-        public bool IsInput
-        {
-            get
-            {
-                return !Mathf.Approximately(input.magnitude, 0f);
-            }
-        }
-
-        public bool CanSprint
-        {
-            get
-            {
-                return canSprint;
-            }
-        }
         // Update is called once per frame
         void Update()
         {
-            input.Set(
-                      Input.GetAxis("Horizontal"),
-                      0,
-                      Input.GetAxis("Vertical")
-                      );
+            movementInput.Set(Input.GetAxis("Horizontal"),0,Input.GetAxis("Vertical"));
 
             canSprint = Input.GetKey(KeyCode.LeftShift);
 
-            if(Input.GetMouseButton(0) && !isAttacking)
+            isLeftMouseClick = Input.GetMouseButton(0);
+            if (isLeftMouseClick)
             {
-                StartCoroutine(HandleAttackInput());
+                HandleLeftMouseClick();
             }
+
+            isInteracting = Input.GetKeyDown(KeyCode.E);
+            if (isInteracting)
+            {
+                HandleInteraction();
+            }
+
+        }
+
+        private void HandleInteraction()
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            bool isHit = Physics.Raycast(ray, out RaycastHit hit, interactableDistance);
+            if (isHit && hit.collider.CompareTag("QuestGiver"))
+            {
+                isTalking = true;
+            }
+        }
+
+        private void HandleLeftMouseClick()
+        {
+            if (!isAttacking)
+                StartCoroutine(HandleAttackInput());
         }
 
         IEnumerator HandleAttackInput()
